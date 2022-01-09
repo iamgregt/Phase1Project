d83790a3fac4d07b3ebe3539ece31ad5df036333
GregT <taylor.gregory2@gmail.com>
2022-01-09 16:22:21 -0700

    Stat values rendered

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+
 document.addEventListener("DOMContentLoaded", () => {
     const playerOne = document.getElementById("player1")
+    const p1Points = document.getElementById("p1points")
+    const p1Rbds = document.getElementById("p1rbds")
 
     fetch('https://www.balldontlie.io/api/v1/players/2931')
     .then(resp => resp.json())
@@ -8,4 +11,37 @@ document.addEventListener("DOMContentLoaded", () => {
         playerOne.innerHTML = player1FullName
 
     })
+
+    fetch('https://www.balldontlie.io/api/v1/season_averages?season=1990&player_ids[]=2931')
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data.data[0].pts)
+        let points = data.data[0].pts
+        let rbds = data.data[0].reb
+        p1Points.innerHTML = `Points = ${points}`
+        p1Rbds.innerHTML = `Rebounds = ${rbds}`
+    })
+
+    renderStats(2931)
 })
+
+function renderStats(player){
+    const playerOne = document.getElementById("p1Object")
+    
+
+
+    fetch(`https://www.balldontlie.io/api/v1/season_averages?season=1990&player_ids[]=${player}`)
+    .then(resp => resp.json())
+    .then(data => {
+
+        console.log(data.data[0])
+        let stats = Object.values(data.data[0])
+        console.log(stats)
+        stats.map(stat => {
+            const newLi = document.createElement('li')
+            playerOne.appendChild(newLi)
+            newLi.innerHTML += stat   
+        })
+    })
+}
+
