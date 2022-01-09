b22b6e3e4270b49c09efe652c416895018d2a8e4
GregT <taylor.gregory2@gmail.com>
2022-01-09 16:34:01 -0700

    added collapsible

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ document.addEventListener("DOMContentLoaded", () => {
     const playerOne = document.getElementById("player1")
     const p1Points = document.getElementById("p1points")
     const p1Rbds = document.getElementById("p1rbds")
+    var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
 
     fetch('https://www.balldontlie.io/api/v1/players/2931')
     .then(resp => resp.json())
@@ -12,15 +26,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
     })
 
-    fetch('https://www.balldontlie.io/api/v1/season_averages?season=1990&player_ids[]=2931')
-    .then(resp => resp.json())
-    .then(data => {
-        console.log(data.data[0].pts)
-        let points = data.data[0].pts
-        let rbds = data.data[0].reb
-        p1Points.innerHTML = `Points = ${points}`
-        p1Rbds.innerHTML = `Rebounds = ${rbds}`
-    })
+    // fetch('https://www.balldontlie.io/api/v1/season_averages?season=1990&player_ids[]=2931')
+    // .then(resp => resp.json())
+    // .then(data => {
+    //     console.log(data.data[0].pts)
+    //     let points = data.data[0].pts
+    //     let rbds = data.data[0].reb
+    //     p1Points.innerHTML = `Points = ${points}`
+    //     p1Rbds.innerHTML = `Rebounds = ${rbds}`
+    // })
 
     renderStats(2931)
 })
